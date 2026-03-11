e0a8920d45da56d989edb864022b960a9e3912d6
niki2422 <randazzonico55@gmail.com>
2026-03-11 13:22:49 -0300

    se creo el menu hamburguesa

--- react-resto/src/components/Navbar.jsx
+++ react-resto/src/components/Navbar.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react"
 import "../styles/navbar.css"
 
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="logo">Resto</div>
 
-      <ul className="nav-links">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Gallery</li>
-        <li>Contact</li>
+function Navbar(){
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return(
+    <nav className="navbar">
+
+      <div className="logo">
+        RestoReact
+      </div>
+
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+
+        <li>
+          <a href="#hero" onClick={() => setMenuOpen(false)}>Inicio</a>
+        </li>
+
+        <li>
+          <a href="#about" onClick={() => setMenuOpen(false)}>Nosotros</a>
+        </li>
+
+        <li>
+          <a href="#menu" onClick={() => setMenuOpen(false)}>Menú</a>
+        </li>
+
+        <li>
+          <a href="#gallery" onClick={() => setMenuOpen(false)}>Galería</a>
+        </li>
+
+        <li>
+          <a href="#contact" onClick={() => setMenuOpen(false)}>Contacto</a>
+        </li>
+
       </ul>
+
+      <div
+        className="hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
     </nav>
   )
 }

3d8bdece4d15d82b5ec012e194e4a2d9b115cd1d
niki2422 <randazzonico55@gmail.com>
2026-03-11 14:52:54 -0300

    se modifico el style del hero

--- react-resto/src/components/Gallery.jsx
+++ react-resto/src/components/Gallery.jsx
@@ -3,12 +3,12 @@ import "../styles/gallery.css";
 function Gallery() {
 
   const images = [
-    "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    "https://images.unsplash.com/photo-1544025162-d76694265947",
-    "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38",
-    "https://images.unsplash.com/photo-1504674900247-0877df9cc836",
-    "https://images.unsplash.com/photo-1523986371872-9d3ba2e2f642",
-    "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4"
+    "../public/images/galeria-comida-1.jpg",
+    "../public/images/galeria-comida-2.jpg",
+    "../public/images/galeria-comida-3.jpg",
+    "../public/images/galeria-comida-4.jpg",
+    "../public/images/galeria-sala-1.jpg",
+    "../public/images/react-resto-2.png"
   ];
 
   return (
--- react-resto/src/components/Hero.jsx
+++ react-resto/src/components/Hero.jsx
@@ -4,13 +4,17 @@ function Hero() {
   return (
     <section id="Hero" className="hero" data-aos="fade-up">
 
+      <div className="hero-overlay"></div>
+
       <div className="hero-content">
 
         <h1>Auténtica Cocina Italiana</h1>
 
         <p>Pasta fresca hecha cada día</p>
 
-        <button>Ver menú</button>
+        <a href="#Menu" className="hero-btn">
+          Ver menú
+        </a>
 
       </div>
 
